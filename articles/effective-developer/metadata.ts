75a63edcb309225531a12ff9c5cd00c9218566fe
dwild8 <dominic55555@hotmail.com>
2025-05-18 22:04:19 +0100

    Typo

--- articles/effective-developer/metadata.ts
+++ articles/effective-developer/metadata.ts
@@ -3,7 +3,7 @@ import {ArticleMetaData} from "@/articles/getArticles";
 const metadata: ArticleMetaData = {
     title: "Action, Openness, Purpose: Three Key Behaviors of Effective Developers",
     description: "Over the years, I've noticed a few simple behaviors that make developers genuinely effective—not just skilled, but impactful. In this piece, I reflect on what I've seen work: taking action, communicating openly, and practicing with real purpose. It's a personal look at lessons that have shaped how I approach my own work.",
-    tags: ["Developer", "Practices", "Effectivenss"],
+    tags: ["Developer", "Practices", "Effectiveness"],
     date: new Date("28 Apr 2025"),
     image: "https://supernotes-resources.s3.amazonaws.com/image-uploads/787d5982-c311-48bd-b852-0f9017a0e0e2--image.png",
     show: true
